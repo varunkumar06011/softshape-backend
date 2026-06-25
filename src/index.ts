d7df3a6c606f4dc0588f5c2c4d438d18fb01a189
Varun Kumar <kirankumarchowdary637@gmail.com>
2026-06-25 23:29:08 +0530

    fix: allow Tauri app origin tauri://localhost in CORS

--- src/index.ts
+++ src/index.ts
@@ -72,6 +72,7 @@ const DEFAULT_ALLOWED_ORIGINS = [
   "http://127.0.0.1:3000",
   "http://localhost:3000",
   "http://localhost:5174",
+  "tauri://localhost",
 ];
 
 function getAllowedOrigins(): string[] {
